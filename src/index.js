43d24d21a8bf64eebd9593429e9edc90811ddc36
Seth <sethyuan@gmail.com>
2022-11-06 21:50:49 +0800

    feat: collecting reminder data and listen for changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "@logseq/libs"
+import { format } from "date-fns"
 import { setup, t } from "logseq-l10n"
 import zhCN from "./translations/zh-CN.json"
 
@@ -20,13 +21,74 @@ async function main() {
     type: "module",
   })
 
+  const futureReminders = await fetchFutureReminders()
+  for (const reminder of futureReminders) {
+    worker.postMessage({ id: reminder.id, contentNew: reminder.content })
+  }
+
+  const cache = new Map()
+  const dbOff = logseq.DB.onChanged(({ txData }) => {
+    for (const [e, a, v, , isAdded] of txData) {
+      switch (a) {
+        case "scheduled":
+        case "deadline": {
+          const item = cache.get(e)
+          if (item) {
+            item.check = true
+          } else {
+            cache.set(e, { check: true })
+          }
+          break
+        }
+        case "content": {
+          const item = cache.get(e)
+          if (item) {
+            if (isAdded) {
+              item.contentNew = v
+            } else {
+              item.contentOld = v
+            }
+          } else {
+            cache.set(e, isAdded ? { contentNew: v } : { contentOld: v })
+          }
+          break
+        }
+      }
+    }
+
+    for (const [id, { check, contentOld, contentNew }] of cache.entries()) {
+      if (check && (contentOld || contentNew)) {
+        worker.postMessage({ id, contentOld, contentNew })
+      }
+    }
+
+    cache.clear()
+  })
+
   logseq.beforeunload(() => {
+    dbOff()
     worker.terminate()
   })
 
-  // TODO: listen for transactions.
-
   console.log("#reminder loaded")
+}
+
+async function fetchFutureReminders() {
+  try {
+    return (
+      await logseq.DB.datascriptQuery(
+        `[:find (pull ?b [:db/id :block/content])
+        :in $ ?now
+        :where
+        (or [?b :block/scheduled ?d] [?b :block/deadline ?d])
+        [(>= ?d ?now)]]`,
+        format(new Date(), "yyyyMMdd"),
+      )
+    ).flat()
+  } catch (err) {
+    console.error(err)
+  }
+  return []
 }
 
 logseq.ready(main).catch(console.error)
--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,7 @@
+const reminders = new Map()
+
 onmessage = (e) => {
   // TODO
+  // const {id, contentOld, contentNew} = e.data
   console.log(e.data)
 }
